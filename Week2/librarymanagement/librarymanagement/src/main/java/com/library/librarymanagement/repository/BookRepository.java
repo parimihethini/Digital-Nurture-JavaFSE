1b91d02a929b06d6cea12614342759f8c4283dfa
Hethini Chowdary <hethinichowdary@gmail.com>
2026-07-04 11:41:40 +0530

    Completed Week 2 Spring Boot Library Management

---
+++ Week2/librarymanagement/librarymanagement/src/main/java/com/library/librarymanagement/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.library.librarymanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.library.librarymanagement.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
