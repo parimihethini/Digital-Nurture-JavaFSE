b797dac5e0082d07941e20c8b0f03e729e2be3c6
Hethini Chowdary <hethinichowdary@gmail.com>
2026-07-11 00:07:22 +0530

    Completed Week3 Spring REST Handson

---
+++ Week3/springresthandson/src/main/java/com/library/springresthandson/SpringresthandsonApplication.java
@@ -0,0 +1,13 @@
+package com.library.springresthandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringresthandsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringresthandsonApplication.class, args);
+    }
+
+}
---
+++ Week3/springresthandson/src/test/java/com/library/springresthandson/SpringresthandsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.springresthandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringresthandsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
