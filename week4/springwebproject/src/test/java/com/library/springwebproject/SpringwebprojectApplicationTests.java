461db2dd6bc2cf824840646ed4651857c64b5e62
Hethini Chowdary <hethinichowdary@gmail.com>
2026-07-15 16:45:14 +0530

    Completed Week 4 Spring Boot exercises

---
+++ week4/springwebproject/src/test/java/com/library/springwebproject/SpringwebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.springwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
